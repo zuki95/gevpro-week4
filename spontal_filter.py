951eabb4b4f33fb8176f9859f95ff3caf4eeb4c6
Kamil <kamilski95@hotmail.com>
2015-02-25 18:25:57 +0100

    Final version of spontal_filter.py

--- spontal_filter.py
+++ spontal_filter.py
@@ -1,38 +1,33 @@
-#!/usr/bin/env python
-#spontan_filter.py
+#!/usr/bin/python3
+#Kamil Zukowski, 25-02-2015
+#spontal_filter.py
 
 import sys
 
-def main():
+def main(argv):
+	
+	source = argv[1]
+	destination = argv[2]
+	
 	import xml.etree.ElementTree as ET
-	tree = ET.parse('spontal.xml')
+	tree = ET.parse(source)
 	root = tree.getroot()
 
-	for data in root.findall('spontal'):
+	for POINT in root.findall('POINT'):
 		
-		BOTTOM_HZ = int(data.find('BOTTOM_HZ').text)
-		TOP_HZ = int(data.find('TOP_HZ').text)
-		F0_START = int(data.find('F0_START').text)
-		F0_END = int(data.find('F0_END').text)
-		if F0_START < BOTTOM_HZ:
-			root.remove(data)
-		elif F0_END > TOP_HZ:
-			root.remove(data)
-		else:
-			tree.write('spontal_filtered.xml')
-			
-			
-#def main(argv):
+		BOTTOM_HZ = float(POINT.find('BOTTOM_HZ').text)
+		TOP_HZ = float(POINT.find('TOP_HZ').text)
+		F0_START = float(POINT.find('F0_START').text)
+		F0_END = float(POINT.find('F0_END').text)
+		
+		if((F0_START < BOTTOM_HZ) or (F0_START > TOP_HZ) or (F0_END < BOTTOM_HZ) or (F0_END > TOP_HZ)): 
+			root.remove(POINT)
 	
-	#if len(sys.argv) == 4:
-		#continue
-		
-	#else:
-		#print("Usage: $ python Arg1, Arg2, Arg3", file=sys.stderr)
-		
-		
+	tree.write(destination)
+			
+			
 if __name__ == "__main__":
-		main()
+		main(sys.argv)
 		
 	
 			
