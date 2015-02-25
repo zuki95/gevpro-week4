46333579c4b4ca3ae5a1e313d72567d05e2b9e0b
Kamil <kamilski95@hotmail.com>
2015-02-25 21:34:51 +0100

    Final version of spontal_filter.py

--- spontal_filter.py
+++ spontal_filter.py
@@ -5,24 +5,24 @@
 import sys
 
 def main(argv):
-	
+	#Input file is argv[1] and destination file is argv[2]
 	source = argv[1]
 	destination = argv[2]
-	
+	#Imports xml file
 	import xml.etree.ElementTree as ET
 	tree = ET.parse(source)
 	root = tree.getroot()
-
+    #Iterates over the points in the corpus of the file
 	for POINT in root.findall('POINT'):
 		
 		BOTTOM_HZ = float(POINT.find('BOTTOM_HZ').text)
 		TOP_HZ = float(POINT.find('TOP_HZ').text)
 		F0_START = float(POINT.find('F0_START').text)
 		F0_END = float(POINT.find('F0_END').text)
-		
+	#Checks whether values of F0_START and F0_END are between the limits of HZ		
 		if((F0_START < BOTTOM_HZ) or (F0_START > TOP_HZ) or (F0_END < BOTTOM_HZ) or (F0_END > TOP_HZ)): 
 			root.remove(POINT)
-	
+	#Writes correct data to output
 	tree.write(destination)
 			
 			
