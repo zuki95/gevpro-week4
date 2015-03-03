e89b52ecb66c7a8c9dc0335eae5c87e2d1a932bd
Kamil <kamilski95@hotmail.com>
2015-03-03 17:14:37 +0100

    Final version of blood.py

--- blood.py
+++ blood.py
@@ -7,6 +7,7 @@ import json
 from collections import namedtuple
 
 def main():
+	#Opens json file, creates an empty list and defines the namedtuple
 	in_file = json.load(open("blood-die.json"))
 	list_match = []
 	matches = namedtuple('matches', 'language, classifications')
@@ -19,12 +20,12 @@ def main():
 		
 		list_blood = word_blood.strip().split(", ")
 		list_die = word_die.strip().split(", ")
-		
+	#Finds matches of words and appends them	
 		for word_blood in list_blood:
 			for word_die in list_die:
 				if(word_blood == word_die):
 					list_match.append(matches(name, _class))
-					
+	#Prints the result using a list comprehension				
 	print([language for (language, classifications) in list_match])
 	
 if __name__ == "__main__":
